a102c35d126be0a4e90a1eeacf1269ce9633d2f8
Airscript <francesco@airscript.it>
2022-08-13 13:53:57 +0000

    feat(scripts): add ci scripts

---
+++ scripts/ci/verify.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ "$1" = "build" ]; then
+  apk --version
+  
+elif [ "$1" = "deploy" ]; then
+  apk --version
+  node -v
+  npm -v
+
+elif [ "$1" = "publish" ]; then
+  apk --version
+
+else 
+  echo ERROR: Environment not found.
+  exit 1
+fi
