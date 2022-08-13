53ae847a4065c584e42a5399375c7b3a9e2331a6
Airscript <dev.airscript@gmail.com>
2022-08-13 17:49:58 +0000

    feat(tests): add base tests

---
+++ tests/mocks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+readonly INSTALL_BASH="Bash installed successfully."
+readonly INSTALL_DOCKER_CLI="Docker CLI installed successfully."
+readonly ADD_GIT_SUBMODULE_SKIPPED="Git submodule update skipped."
+readonly ADD_GIT_SUBMODULE_EXECUTED="Git submodule update executed."
