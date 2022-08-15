6afcf7b3d8f262d39ccd1c79cd52466a44961dee
Airscript <francesco@airscript.it>
2022-08-15 17:44:44 +0000

    feat(scripts): add base verify in verify script

--- scripts/ci/verify.sh
+++ scripts/ci/verify.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-if [ "$1" = "build" ]; then
+if [ "$1" = "base" ]; then
   apk --version
+
+elif [ "$1" = "build" ]; then
+  apk --version
+  git --version
+  hugo version
   
 elif [ "$1" = "deploy" ]; then
   apk --version
   node -v
   npm -v
+  netlify -v
 
 elif [ "$1" = "publish" ]; then
   apk --version
+  docker -v
 
 else 
   echo ERROR: Environment not found.
