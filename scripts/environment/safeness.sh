eeeae2293decf95d80b06bc193fd41328d24b9fa
Airscript <francesco@airscript.it>
2022-12-25 16:02:24 +0100

    feat(scripts): add environment safeness script

---
+++ scripts/environment/safeness.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global --add safe.directory /workspaces/blog.airscript.it
+git config --global --add safe.directory /workspaces/blog.airscript.it/tests/bats
+git config --global --add safe.directory /workspaces/blog.airscript.it/themes/PaperMod
+git config --global --add safe.directory /workspaces/blog.airscript.it/tests/libraries/bats-assert
+git config --global --add safe.directory /workspaces/blog.airscript.it/tests/libraries/bats-support
